e4af9bfb7ed93e4c34c4d844756824d28759a5ff
Douglas Meehan <dmeehan@gmail.com>
2014-04-27 13:14:52 -0400

    django secret key

--- spontaneousurbanplants/settings/production.py
+++ spontaneousurbanplants/settings/production.py
@@ -1,3 +1,62 @@
+import os
+
+from base import *
+
+
+#==============================================================================
+# Site
+#==============================================================================
+# See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
+ALLOWED_HOSTS = ['spontaneousurbanplants.org', 'www.spontaneousurbanplants.org', 'smtp.webfaction.com']
+
+
+#==============================================================================
+# Email
+#==============================================================================
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host
+EMAIL_HOST = 'smtp.webfaction.com'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-password
+EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_HOST_PASSWORD', '')
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-user
+EMAIL_HOST_USER = 'futuregreen'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
+EMAIL_PORT = '587'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
+EMAIL_USE_TLS = 'True'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
+EMAIL_SUBJECT_PREFIX = '[%s] ' % SITE_NAME
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#server-email
+SERVER_EMAIL = 'admin@spontaneousurbanplants.org'
+
+DEFAULT_FROM_EMAIL = 'Spontaneous Urban Plants <info@spontaneousurbanplants.org>'
+
+
+#==============================================================================
+# Installed Apps
+#==============================================================================
+
+
+
+#==============================================================================
+#  Celery
+#==============================================================================
+
+
+BROKER_URL = "redis://localhost:6379/0"
+
+#==============================================================================
+#  Databases
+#==============================================================================
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
@@ -8,5 +67,3 @@ DATABASES = {
         'PORT': '',
     }
 }
-
-BROKER_URL = "redis://localhost:6379/0"
