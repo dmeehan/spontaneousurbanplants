2d6e47baf8d285b6390d77fbfa12c8ca76694dac
Douglas Meehan <dmeehan@gmail.com>
2014-04-27 19:52:35 -0400

    work in progress

--- spontaneousurbanplants/apps/instamedia/utils.py
+++ spontaneousurbanplants/apps/instamedia/utils.py
@@ -1,10 +1,9 @@
-#instamedia/utils.py
-from django.core.urlresolvers import reverse_lazy
-from django.http import HttpRequest
+# instamedia/utils.py
+from django.core.urlresolvers import reverse
 
 def get_callback_url():
-	return HttpRequest.build_absolute_uri(reverse_lazy('instagram_callback'))
+	reverse('instagram_callback'))
 
 
 def get_realtime_callback_url():
-	return HttpRequest.build_absolute_uri(reverse_lazy('instagram_realtime_callback'))
+	reverse('instagram_realtime_callback'))
