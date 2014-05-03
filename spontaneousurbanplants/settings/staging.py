ecddc9724e62fefb0c6c88be458cec2d01dabc33
Douglas Meehan <dmeehan@gmail.com>
2014-05-03 16:04:49 -0400

    work in progress

--- spontaneousurbanplants/settings/staging.py
+++ spontaneousurbanplants/settings/staging.py
@@ -1,6 +1,14 @@
 from production import *
 
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
 
 INSTALLED_APPS += (
 	'debug_toolbar',  
 )
+
+INTERNAL_IPS = ('127.0.0.1', 'spontaneousurbanplants.org')
+
