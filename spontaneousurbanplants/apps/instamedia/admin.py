e9504343ee3b262b4b8654d7081c2fc01af44965
Douglas Meehan <dmeehan@gmail.com>
2014-05-04 12:48:54 -0400

    added custom action to verify images

--- spontaneousurbanplants/apps/instamedia/admin.py
+++ spontaneousurbanplants/apps/instamedia/admin.py
@@ -5,9 +5,15 @@ from django.contrib import messages
 
 from .models import InstagramImage, InstagramTag
 
+def make_verified(modeladmin, request, queryset):
+    queryset.update(verified=True)
+
+make_verified.short_description = "Mark selected images as verified"
+
 class InstagramImageAdmin(admin.ModelAdmin):
     list_display = ['thumbnail', 'remote_id', 'created', 'updated', 'last_synced', 'verified']
     list_editable = ['verified']
+    actions = [make_verfied]
 
 class InstagramTagAdmin(admin.ModelAdmin):
     list_display = ['name', 'sync', 'subscribe', 'moderate']
