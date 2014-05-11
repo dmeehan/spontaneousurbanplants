bc586c4ee4a87416d3a63211f140cfc9943a7962
Douglas Meehan <dmeehan@gmail.com>
2014-05-11 00:01:08 -0400

    added notificartions

--- spontaneousurbanplants/settings/staging.py
+++ spontaneousurbanplants/settings/staging.py
@@ -15,5 +15,5 @@ INSTALLED_APPS += (
 
 INTERNAL_IPS = ('127.0.0.1',)
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
