4b986c2aa3ad9134617adaa2c51461469500a0cf
Douglas Meehan <dmeehan@gmail.com>
2014-05-11 00:10:24 -0400

    added notificartions

--- spontaneousurbanplants/settings/staging.py
+++ spontaneousurbanplants/settings/staging.py
@@ -5,13 +5,14 @@ DEBUG=True
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'middleware.ExceptionLoggingMiddleware'
 )
 
 
 INSTALLED_APPS += (
 	'debug_toolbar',  
 )
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
 INTERNAL_IPS = ('127.0.0.1',)
 
