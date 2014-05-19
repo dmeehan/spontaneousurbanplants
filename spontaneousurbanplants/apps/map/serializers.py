933942b049a96ecf8c1a4c14c4f13805fbf0e352
Douglas Meehan <dmeehan@gmail.com>
2014-05-18 20:21:50 -0400

    filtering by tag

--- spontaneousurbanplants/apps/map/serializers.py
+++ spontaneousurbanplants/apps/map/serializers.py
@@ -1,12 +1,14 @@
 # map/serializers.py
 
+from rest_framework import serializers
 from rest_framework_gis import serializers as gis_serializers
 
 from apps.instamedia.models import InstagramImage
 
 class ImageSerializer(gis_serializers.GeoFeatureModelSerializer):
+	tags = serializers.SlugRelatedField(many=True, read_only=True,
+                                        slug_field='name')
 	class Meta:
 		model = InstagramImage
 		geo_field = 'coordinates'
 
-		fields = ()
