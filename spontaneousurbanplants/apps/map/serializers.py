802ee86f09d73350e8017655fe310fae4d54ad6c
Douglas Meehan <dmeehan@gmail.com>
2014-05-27 01:04:48 -0400

    css

--- spontaneousurbanplants/apps/map/serializers.py
+++ spontaneousurbanplants/apps/map/serializers.py
@@ -4,11 +4,21 @@ from rest_framework import serializers
 from rest_framework_gis import serializers as gis_serializers
 
 from apps.instamedia.models import InstagramImage
+from apps.plants.models import Plant
 
 class ImageSerializer(gis_serializers.GeoFeatureModelSerializer):
 	tags = serializers.SlugRelatedField(many=True, read_only=True,
                                         slug_field='name')
+
+	plant = serializers.SerializerMethodField('get_plant')
+
 	class Meta:
 		model = InstagramImage
 		geo_field = 'coordinates'
 
+	def get_plant(self, obj):
+		for tag in obj.tags.all():
+			try:
+				return Plant.objects.get(hashtag__iexact=tag.name)
+			except Plant.DoesNotExist:
+				return ""
