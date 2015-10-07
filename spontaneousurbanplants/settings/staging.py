75213bfa872d9e2c6a166619d32c9fdbd8f3f7fe
Douglas Meehan <dmeehan@gmail.com>
2015-10-06 22:54:59 -0400

    new staging settings

--- spontaneousurbanplants/settings/staging.py
+++ spontaneousurbanplants/settings/staging.py
@@ -1,18 +1,61 @@
-from production import *
+import os
 
-DEBUG=True
+from base import *
 
-# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
+#==============================================================================
+# Debugging
+#==============================================================================
+
+DEBUG = True
+
+#==============================================================================
+# Site
+#==============================================================================
+# See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
+ALLOWED_HOSTS = ['test.spontaneousurbanplants.org']
 
 
-INSTALLED_APPS += (
-	'debug_toolbar',  
-)
+##==============================================================================
+# Email Configuration
+#==============================================================================
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
-INTERNAL_IPS = ('127.0.0.1',)
+#==============================================================================
+# Installed Apps
+#==============================================================================
+
+#INSTALLED_APPS += (
+	#'debug_toolbar',  
+#)
+
+#DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+#==============================================================================
+#  Databases
+#==============================================================================
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.environ.get('DJANGO_DB_NAME', ''),
+        'USER': os.environ.get('DJANGO_DB_USER', ''),
+        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', ''),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+#==============================================================================
+# Caching
+#==============================================================================
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+
+
 
