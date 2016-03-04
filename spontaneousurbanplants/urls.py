e324e4ddd48f0a28af9c3ef89832578eaa8613b5
Douglas Meehan <dmeehan@gmail.com>
2016-03-04 00:32:03 -0500

    fix for randomizing image list

--- spontaneousurbanplants/urls.py
+++ spontaneousurbanplants/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 from django.views.generic import TemplateView
 
-from .views import LatestImagesWithBookView
+from spontaneousurbanplants.views import LatestImagesWithBookView
 
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
