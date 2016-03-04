0c3c72ce934c9f7f34ebd27031a59791c5508ffa
Douglas Meehan <dmeehan@gmail.com>
2016-03-04 00:39:34 -0500

    fix for randomizing image list

--- spontaneousurbanplants/urls.py
+++ spontaneousurbanplants/urls.py
@@ -26,7 +26,7 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', include('apps.instamedia.urls')),
+    url(r'^$', include('apps.home.urls')),
 
     url(r'^feed/', include('apps.instamedia.urls')),
 
